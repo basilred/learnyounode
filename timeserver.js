c893d5a687634bc2839a317e3f043f3db8fb45a8
Vassily Krasnov <mykrasnov@gmail.com>
2014-11-01 23:08:55 +0300

    Add socket.end() function.

--- timeserver.js
+++ timeserver.js
@@ -1,7 +1,17 @@
 var net = require('net');
 
 var server = net.createServer(function (socket) {
-	// body...
+	socket.end(function () {
+		var date = new Date();
+		var datestr = date.getFullYear() +
+			"-" +
+			date.getMonth() +
+			"-" +
+			date.getDate();
+		console.log(datestr);
+		// return datestr;
+	});
 });
 
+// Use the port number supplied as the first command-line argument.
 server.listen(process.argv[2]);
