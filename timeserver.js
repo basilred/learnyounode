2947f96f1570de5b933749a83f9179d88fc3b25e
Vassily Krasnov <mykrasnov@gmail.com>
2014-11-02 00:50:48 +0300

    No UTC time and function for leading zero.

--- timeserver.js
+++ timeserver.js
@@ -1,8 +1,19 @@
 var net = require('net');
 
+// Adding a leading zero to text representation
+function pad (number) {
+	if (number < 10) {
+		return '0' + number;
+	}
+	return number;
+}
+
 var server = net.createServer(function (socket) {
-	var date = new Date().toISOString();
-	socket.end(date);
+	var date = new Date();
+	var datestr = date.getFullYear()+'-'+pad(date.getMonth()+1)+'-'+
+		pad(date.getDate()) + ' ' +
+		pad(date.getHours()) + ':' + pad(date.getMinutes());
+	socket.end(datestr);
 });
 
 // Use the port number supplied as the first command-line argument.
