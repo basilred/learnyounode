c5b9ae8859d16b85a3a4886b0274e31564540d25
Vassily Krasnov <mykrasnov@gmail.com>
2014-11-02 02:28:26 +0300

    Listening port provided by the command-line first argument.

--- http-file-server.js
+++ http-file-server.js
@@ -1,5 +1,7 @@
-var http = require('http');
+var http = require('http'),
+	listeningPort = process.argv[2];
 var server = http.createServer(function (request, response) {
 	// body...
 });
-server.listen(8080);
+console.log('Listen to on port ', listeningPort);
+server.listen(listeningPort);
