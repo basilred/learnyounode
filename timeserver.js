fc433fabf6fe8091a3b71545256c27f90a921fcc
Vassily Krasnov <mykrasnov@gmail.com>
2014-11-02 00:06:38 +0300

    Create and use an ISO string.

--- timeserver.js
+++ timeserver.js
@@ -1,16 +1,8 @@
 var net = require('net');
 
 var server = net.createServer(function (socket) {
-	socket.end(function () {
-		var date = new Date();
-		var datestr = date.getFullYear() +
-			"-" +
-			date.getMonth() +
-			"-" +
-			date.getDate();
-		console.log(datestr);
-		// return datestr;
-	});
+	var date = new Date().toISOString();
+	socket.end(date);
 });
 
 // Use the port number supplied as the first command-line argument.
